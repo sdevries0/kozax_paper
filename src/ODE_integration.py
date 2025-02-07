d16311a61a2a5c632f67084f3c1e9c90e3a46b86
sdevries0 <sigur.devries@ru.nl>
2025-02-07 11:56:57 +0100

    update LV experiment results

--- src/ODE_integration.py
+++ src/ODE_integration.py
@@ -164,14 +164,14 @@ def evolve_LV():
             ("+", lambda x, y: jnp.add(x, y), 2, 0.5), 
             ("-", lambda x, y: jnp.subtract(x, y), 2, 0.1), 
             ("*", lambda x, y: jnp.multiply(x, y), 2, 0.5), 
-            # ("**", lambda x, y: jnp.power(x, y), 2, 0.1), 
-            # ("/", lambda x, y: jnp.divide(x, y), 2, 0.1)
+            ("**", lambda x, y: jnp.power(x, y), 2, 0.1), 
+            ("/", lambda x, y: jnp.divide(x, y), 2, 0.1)
         ]
 
     variable_list = [["x" + str(i) for i in range(env.n_var)]]
 
-    population_size = 200
-    num_populations = 10
+    population_size = 400
+    num_populations = 25
     num_generations = 100
 
     fitness_function = Evaluator(solver=diffrax.Dopri5(), dt0 = 0.01, stepsize_controller=diffrax.PIDController(atol=1e-6, rtol=1e-6, dtmin=0.001), max_steps=300, optimize_dimensions = jnp.array([0]))
@@ -179,7 +179,7 @@ def evolve_LV():
     layer_sizes = jnp.array([env.n_var])
 
     strategy = GeneticProgramming(num_generations, population_size, fitness_function, operator_list, variable_list, layer_sizes, num_populations = num_populations, backend='gpu',
-                            max_nodes = 15, migration_period=10, coefficient_optimisation="ES", ES_n_offspring = 40, ES_n_iterations = 5, size_parsimony=0.003, 
+                            max_nodes = 15, migration_period=5, coefficient_optimisation="ES", ES_n_offspring = 40, ES_n_iterations = 5, size_parsimony=0.003, 
                             start_coefficient_optimisation = 0, optimise_coefficients_elite=500, init_learning_rate=0.2, migration_percentage=0.05)
 
     seeds = jnp.arange(10)
@@ -200,6 +200,9 @@ def evolve_LV():
                 end_ts = ts.shape[0]
             key, eval_key, sample_key = jr.split(key, 3)
             fitness, population = strategy.evaluate_population(population, (x0s, ts[:end_ts], ys[:,:end_ts]), eval_key)
+
+            best_fitness, best_solution = strategy.get_statistics(g)
+            print(f"In generation {g+1}, best fitness = {best_fitness:.4f}, best solution = {strategy.to_string(best_solution)}")
 
             if g < (num_generations-1):
                 population = strategy.evolve(population, fitness, sample_key)
